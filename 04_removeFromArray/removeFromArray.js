204fdb8254e44a23e4313e1b0c3cbdc362f101e7
Jona <jonathan.cortes.v@gmail.com>
2025-03-22 17:06:44 -0300

    Se modifican archivos removeFromArray.js y removeFromArray.spec.js
    Se escribe la función y se comprueba que se cumplan los test correctamente.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (vector, ...elements) {
+  for (let element of elements) {
+    for (let i = 0; i < vector.length; i++) {
+      if (vector[i] === element) {
+        vector.splice(i, 1);
+        i--;
+      } else {
+        continue;
+      }
+    }
+  }
+  return vector;
 };
 
 // Do not edit below this line
