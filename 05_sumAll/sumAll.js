a2642b348fcb6894bd7488c020d0db4c9788f8e9
Jona <jonathan.cortes.v@gmail.com>
2025-03-22 17:53:46 -0300

    Se modifican los archivos sumAll.js y sumAll.spec.js.
    Se escribe la función requerida y se verifica que se cumplan exitosamente los test.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function (numberOne, numberTwo) {
+  function isFloat(number) {
+    if (number != Math.floor(number)) {
+      return true;
+    } else {
+      false;
+    }
+  }
 
+  if (
+    typeof numberOne != `number` ||
+    typeof numberTwo != `number` ||
+    numberOne < 0 ||
+    numberTwo < 0 ||
+    isFloat(numberOne) ||
+    isFloat(numberTwo)
+  ) {
+    return `ERROR`;
+  } else {
+    minNumber = Math.min(numberOne, numberTwo);
+    maxNumber = Math.max(numberOne, numberTwo);
+  }
+
+  let sum = 0;
+  for (let i = minNumber; i <= maxNumber; i++) {
+    sum = sum + i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
