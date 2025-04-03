9bbb1be48380ef9117006768d69a51308452e204
Jona <jonathan.cortes.v@gmail.com>
2025-04-03 14:52:20 -0300

    08 - Calculator: Se modifican los archivos calculator.js y calculator.spec.js
    Se escriben las funciones solicitadas en el archivo README.md y se verifica que se cumplen los test.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function (numberOne, numberTwo) {
+  return numberOne + numberTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function (numberOne, numberTwo) {
+  return numberOne - numberTwo;
 };
 
-const sum = function() {
-	
+const sum = function (numberArray = [0]) {
+  return numberArray.reduce((totalAcum, currentItem) => {
+    return totalAcum + currentItem;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numberArray = [0]) {
+  return numberArray.reduce((totalAcum, currentItem) => {
+    return totalAcum * currentItem;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (baseNumber, exponentNumber) {
+  return baseNumber ** exponentNumber;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  let result = 1;
+  if (number === 0) {
+    return 1;
+  } else {
+    for (let i = 1; i <= number; i++) {
+      result = result * i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +41,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
