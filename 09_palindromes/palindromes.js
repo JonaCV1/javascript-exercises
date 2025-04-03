1ef818ef2abfa9babf94f9977eb3cbc74e01e956
Jona <jonathan.cortes.v@gmail.com>
2025-04-03 17:23:04 -0300

    09 - Polindromes: Se modifican los archivos palindromes.js y palindromes.spec.js
    Se escribe la función polindromes y se verifica que se cumpla con los test.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
+function isValidChar(char) {
+  let codeValue = char.charCodeAt();
+  if (
+    (codeValue >= 97 && codeValue <= 122) || // letras
+    (codeValue >= 48 && codeValue <= 57) // números
+  ) {
+    return true;
+  }
+}
 
+const palindromes = function (string) {
+  let lowerString = string.toLowerCase();
+  let arrayCharacters = Array.from(lowerString);
+  let validArray = arrayCharacters.filter(isValidChar);
+  let validString = validArray.join("");
+  let reverseArray = validArray.reverse();
+  let validReverseString = reverseArray.join("");
+  console.log(validString);
+  console.log(validReverseString);
+  if (validString === validReverseString) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
