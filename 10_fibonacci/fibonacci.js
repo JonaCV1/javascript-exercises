c0238f71d952580d31330466f339b59202a5ac5f
Jona <jonathan.cortes.v@gmail.com>
2025-04-03 19:03:53 -0300

    10 - Fibonacci: Se modifican los archivos fibonacci.js y fibonacci.spec.js
    Se escribe la función fibonacci y se verifica que se cumplan los test.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (index) {
+  let array = [1, 1];
+  index = index.toString();
+  if (index == 0) {
+    return 0;
+  } else if (index < 0) {
+    return "OOPS";
+  } else {
+    for (let i = 0; i <= index; i++) {
+      array.push(array[i] + array[i + 1]);
+    }
+    return array[index - 1];
+  }
 };
 
 // Do not edit below this line
