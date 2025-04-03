647c1d0e47ffb317d11647612da0170f422043d5
Jona <jonathan.cortes.v@gmail.com>
2025-04-03 20:01:54 -0300

    12 - findTheOldest: Se modifican los archivos findTheOldest.js y findTheOldest.spec.js
    Se escribe la función requerida en el archivo README.md y se comprueban los test.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  let ages = [];
+  let setDate = new Date();
+  let actualYear = setDate.getFullYear();
+  array.forEach((object) => {
+    if (object.yearOfDeath == undefined) {
+      ages.push(actualYear - object.yearOfBirth);
+    } else {
+      ages.push(object.yearOfDeath - object.yearOfBirth);
+    }
+  });
+  let index = ages.indexOf(Math.max(...ages));
+  return array[index];
 };
 
 // Do not edit below this line
